d30820b9b3096425c5183c9193b9be5d026d28d7
Natallia Nikitsina <natiknik73@gmail.com>
2023-07-07 17:58:21 +0300

    qqq

---
+++ task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Задайте массив вещественных чисел.
+ // Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+//float [] CreatArrey(int size, int min, int max)
+//{
+    double [] arrey =  new double[15];
+    Random rand = new Random();
+    for(int i =0; i<arrey.Length; i++)
+    {
+        arrey[i] = rand.NextDouble()*(23-22)+22;
+        
+    }
+    Console.WriteLine($"[{string.Join(", ", arrey)}]");
+    //return arrey;
+//}
+
+//float [] NewArrey = CreatArrey(8, 0, 1);
